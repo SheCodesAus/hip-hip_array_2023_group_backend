9d0b7452270bbc7e2a65989726adf79937c75508
Bbuunn <bunnyryou@gmail.com>
2023-03-18 14:31:40 +1100

    Add users app, create user model, serialize user

--- learnable/learnable/urls.py
+++ learnable/learnable/urls.py
@@ -1,4 +1,4 @@
-"""workshops URL Configuration
+"""learnable URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('workshops.urls')),
+    path('users/', include('users.urls'))
 ]
--- learnable/learnable/wsgi.py
+++ learnable/learnable/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for workshops project.
+WSGI config for learnable project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
