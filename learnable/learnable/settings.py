327825e97b5a6712c4baf6813a5b81fa2db5e949
Bbuunn <bunnyryou@gmail.com>
2023-03-20 21:18:44 +1100

    Add token authentication function

--- learnable/learnable/settings.py
+++ learnable/learnable/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'workshops.apps.WorkshopsConfig',  
     'users.apps.UsersConfig',
     'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -48,6 +49,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles'
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+AUTH_USER_MODEL = 'users.User'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
