8efcaf7a8adef6d79afe3d73082a21cca0357346
Bbuunn <bunnyryou@gmail.com>
2023-03-20 21:19:47 +1100

    Allow account owners to view and update info

--- learnable/users/serializers.py
+++ learnable/users/serializers.py
@@ -20,3 +20,12 @@ class UserSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+
+class UserDetailSerializer(UserSerializer):
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.password = validated_data.get('password', instance.password)
+        instance.save()
+        return instance 
