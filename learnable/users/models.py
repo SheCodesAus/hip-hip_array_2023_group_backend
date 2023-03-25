e6c8a84613e9a64282144d596c94b1f41e7bff82
Bbuunn <bunnyryou@gmail.com>
2023-03-25 11:22:04 +1100

    Working progress of skill function

--- learnable/users/models.py
+++ learnable/users/models.py
@@ -2,26 +2,28 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
+# class Skill(models.Model):
+#     name = models.CharField(max_length=64, unique=True)
+
+#     def __str__(self):
+#         return self.name
+
+
 class User(AbstractUser):
     username = models.EmailField(unique=True, null=True)
     last_login = None
-    first_name = models.CharField(max_length=300)
-    last_name = models.CharField(max_length=300)
-    password = models.CharField(max_length=1000, null=True)
+    is_python_mentor = models.BooleanField(default=False)
+    is_django_mentor = models.BooleanField(default=False)
+    is_react_mentor = models.BooleanField(default=False)
+    is_javascript_mentor = models.BooleanField(default=False)
+    is_htmlcss_mentor = models.BooleanField(default=False)
+
     USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
 
-    skills_choices = (
-        ("python", "Python"),
-        ("django", "Django"),
-        ("react", "React"),
-        ("javascript", "JavaScript"),
-        ("htmlcss", "HTML/CSS"),
-    )
-
-    skills = models.CharField(max_length=100,
-                              choices=skills_choices,
-                              default=None, blank=True, null=True)
+    # skills = models.ManyToManyField(
+    #     Skill, related_name="users", default=None, blank=True
+    # )
 
     def __str__(self):
         return self.username
--- learnable/users/serializers.py
+++ learnable/users/serializers.py
@@ -2,30 +2,48 @@ from rest_framework import serializers
 from .models import User
 
 
-class UserSerializer(serializers.Serializer):
-    id = serializers.ReadOnlyField()
-    username = serializers.EmailField()
-    first_name = serializers.CharField(max_length=300)
-    last_name = serializers.CharField(max_length=300)
-    password = serializers.CharField(max_length=1000)
+# class SkillSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Skill
+#         fields = ['id', 'name']
 
-    skills_choices = (
-        ("python", "Python"),
-        ("django", "Django"),
-        ("react", "React"),
-        ("javascript", "JavaScript"),
-        ("htmlcss", "HTML/CSS"),
-    )
-    skills = serializers.MultipleChoiceField(choices=skills_choices)
+
+class UserSerializer(serializers.ModelSerializer):
+    # skills = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
+    username = serializers.EmailField()
+    is_python_mentor = serializers.BooleanField()
+    is_django_mentor = serializers.BooleanField()
+    is_react_mentor = serializers.BooleanField()
+    is_javascript_mentor = serializers.BooleanField()
+    is_htmlcss_mentor = serializers.BooleanField()
+
+    class Meta:
+        model = User
+        fields = [
+            'id', 'username', 'first_name', 'last_name', 'skills', 'password', 'is_python_mentor', 'is_django_mentor', 'is_react_mentor', 'is_javascript_mentor', 'is_htmlcss_mentor'
+        ]
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
 
+
 class UserDetailSerializer(UserSerializer):
     def update(self, instance, validated_data):
         instance.username = validated_data.get('username', instance.username)
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.first_name = validated_data.get(
+            'first_name', instance.first_name)
+        instance.last_name = validated_data.get(
+            'last_name', instance.last_name)
         instance.password = validated_data.get('password', instance.password)
+        instance.is_python_mentor = validated_data.get(
+            'is_python_mentor', instance.is_python_mentor)
+        instance.is_django_mentor = validated_data.get(
+            'is_django_mentor', instance.is_django_mentor)
+        instance.is_react_mentor = validated_data.get(
+            'is_react_mentor', instance.is_react_mentor)
+        instance.is_javascript_mentor = validated_data.get(
+            'is_javascript_mentor', instance.is_javascript_mentor)
+        instance.is_htmlcss_mentor = validated_data.get(
+            'is_htmlcss_mentor', instance.is_htmlcss_mentor)
         instance.save()
-        return instance 
+        return instance
